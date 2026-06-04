5dfc47c14b9641331c8ac742d99af1c1d2a25663
qazimoizz <qazimoiz222@gmail.com>
2026-06-04 14:48:33 +0500

    added one more

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Counter from "./counter/Counter"
+// import Counter from "./counter/Counter"
 import FormHandle from "./FormHandle"
-import ToggleText from "./ToggleText"
+// import ToggleText from "./ToggleText"
 
 function App() {
 
   return (
     <>
-    <ToggleText/>
-    <Counter/>
-    <br /><br /><br />
+    {/* <ToggleText/>
+    <Counter/> */}
+    {/* <br /><br /><br /> */}
     <FormHandle/>
     </>
   )
--- src/FormHandle.jsx
+++ src/FormHandle.jsx
@@ -1,21 +1,54 @@
 import { useState } from "react";
 
 function FormHandle() {
-  const [text, setText] = useState("");
+  const [name,setName] = useState()
+  const [email,setEmail] = useState()
+  const handleForm = (e)=>{
+    e.preventDefault()
+    console.log("name :", name)
+    console.log("email :", email)
+  }
+  const getName = (e)=>{
+    return setName(e.target.value)
+  }
+  const getEmail = (e)=>{
+    return setEmail(e.target.value)
+  }
 
-  const handleChange = (e) => {
-    setText(e.target.value);
-  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <>
-      <input
-        type="text"
-        onChange={handleChange}
-        placeholder="Type something..."
-      />
+    <h1>React form submit project</h1>
+      <form onSubmit={handleForm}>
+        <label htmlFor="">Name</label>
+        <input type="text" value={name} onChange={getName} />
+      
+      <br /> 
 
-      <p> hello, {text || "guest "}</p>
+        <label htmlFor="">Email</label>
+        <input type="email" value={email} onChange={getEmail} />
+      
+      <button type="submit">Submit</button>
+      </form>
+
+
     </>
   );
 }
