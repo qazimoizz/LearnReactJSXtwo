ed83ff6b2ddf64aa3a3236cd59493f6640783714
qazimoizz <qazimoiz222@gmail.com>
2026-06-07 18:23:17 +0500

    added one more

---
+++ src/advance.js
@@ -0,0 +1,34 @@
+//?objects literals 
+
+let n = "student";
+const obj  ={
+[n]: "qazimoiz",
+course: "b-tech",
+detail :function(){
+return `${this.student} is the student of ${this.course}`
+}
+}
+console.log(obj.detail());
+
+//? destructuring array methods 
+
+const destructure = ["qazi", 21, "BSCS"]
+
+const [name, age, course] = destructure;
+
+console.log(name);
+console.log(age);
+console.log(course);
+
+function myFunc([name, age, city]){
+console.log(name);
+console.log(age);
+console.log(city);
+}
+myFunc(["qazi", 21 ,"karachi"] );
+
+const arr = ["hamza", "BA", "karachi"] 
+
+const  [fname , ...args] = arr;
+console.log(fname);
+console.log(args);
